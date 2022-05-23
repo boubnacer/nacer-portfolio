7ca3f5bee42c65598f33c32d440e0d94d63d3719
boubnacer <nassirox99@gmail.com>
2022-05-23 20:45:46 +0100

    update services

--- src/components.js/Services.js
+++ src/components.js/Services.js
@@ -97,7 +97,7 @@ const Services = () => {
               <h2 className="service__list-icon">
                 <HiOutlineBadgeCheck />
               </h2>
-              <p>HTML/CSS, SASS and TailwindCss</p>
+              <p>HTML5/CSS3, SASS, Bootstrap and TailwindCss</p>
             </li>
             <li>
               <h2 className="service__list-icon">
