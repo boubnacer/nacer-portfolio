0628d183a4a3fa8b913553ad51f45bbeccc5ec45
nacer <nassirox99@gmail.com>
2025-10-27 21:38:14 +0100

    Adding image for light mode

--- src/components.js/Header.js
+++ src/components.js/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import me from "../assets/me.jpg";
 import meLight from "../assets/meLight.jpg";
 import "../templates.js/header.css";
@@ -7,17 +7,41 @@ import { useGlobalContext } from "../context";
 
 const Header = () => {
   const { isDarkMode } = useGlobalContext();
-  
+  const [currentImage, setCurrentImage] = useState(isDarkMode ? me : meLight);
+  const [nextImage, setNextImage] = useState(isDarkMode ? meLight : me);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    const newImage = isDarkMode ? me : meLight;
+    if (newImage !== currentImage) {
+      setNextImage(newImage);
+      setIsTransitioning(true);
+      
+      const timer = setTimeout(() => {
+        setCurrentImage(newImage);
+        setIsTransitioning(false);
+      }, 300);
+      
+      return () => clearTimeout(timer);
+    }
+  }, [isDarkMode, currentImage]);
+
   return (
     <header id="header">
       <div className="header__container">
         <div className="me">
-          <img 
-            key={isDarkMode ? 'dark' : 'light'}
-            className="me_img" 
-            src={isDarkMode ? me : meLight} 
-            alt="me" 
-          />
+          <div className="image-container">
+            <img 
+              className="me_img current" 
+              src={currentImage} 
+              alt="me" 
+            />
+            <img 
+              className={`me_img next ${isTransitioning ? 'fade-in' : ''}`}
+              src={nextImage} 
+              alt="me" 
+            />
+          </div>
         </div>
         <div className="container header__details">
           <h1>Boubkraoui Nacer</h1>
